9dc99e0d61bb858aba510107214fae89f12c860a
Vincexx <cvpitagan@gmail.com>
2020-10-26 12:27:23 +0800

    Edited Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    University Virtual Accreditation
                 </div>
 
                 <div class="links">
