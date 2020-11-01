99d41ef501a948cc6c0f22f6ef550e047739ce21
Vincexx <cvpitagan@gmail.com>
2020-11-01 11:19:14 +0800

    CRUD of Programs

---
+++ resources/js/components/admin/Program.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <v-app>
+
+      <div class="text-center">
+        <v-snackbar
+          v-model="snackbar"
+          :timeout="timeout"
+        >
+          {{ message }}
+
+          <template v-slot:action="{ attrs }">
+            <v-btn
+              color="blue"
+              text
+              v-bind="attrs"
+              @click="snackbar = false"
+            >
+              Close
+            </v-btn>
+          </template>
+        </v-snackbar>
+      </div>
+
+      <v-container>
+        <v-card>
+          <v-card-title>
+            
+            List of Programs
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+            
+          </v-card-title>      
+
+          <v-btn class="mx-2 my-2" fab dark color="indigo" @click.stop="showDialog = true">
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+
+          <v-data-table
+            :headers="headers"
+            :items="programs"
+            :search="search"
+          >
+          
+           <template v-slot:item.actions="{ item }">
+              <v-icon
+                  small
+                  class="mr-2"
+                  color="primary"
+                  @click.stop="showDialog=true, edit=true, editProgram(item.id)"
+              >
+                  mdi-pencil
+              </v-icon>
+
+              <v-icon
+                  color="red"
+                  small
+                  @click="deleteProgram(item.id)"
+              >
+                  mdi-delete
+              </v-icon>
+
+          </template>
+          
+          </v-data-table>
+        </v-card>
+      </v-container>
+    </v-app>
+    <AddProgram :showDialog="showDialog" :program1="program" :edit="edit" />
+  </div>
+</template>
+
+<script>
+  import AddProgram from "./children/AddProgram"
+  export default {
+    mounted () {
+
+      this.$on('hide_dialog', () => {
+          this.showDialog = false
+      })
+
+      this.$on('program_added', (message) => {
+          this.fetchPrograms()
+          this.message = message
+          this.snackbar = true
+          this.showDialog = false
+      })
+
+
+    },
+    components : {
+      AddProgram
+    },
+    props : ['authUser'],
+    data () {
+      return {
+        config : { 
+            'headers': { 
+              'Authorization': 'Bearer ' + this.authUser.api_token,
+              'Accept' : 'application/json',
+            } 
+        },
+        edit : false,
+        snackbar: false,
+        message: '',
+        timeout: 3000,
+        showDialog : false,
+        search: '',
+        programs : [],
+        program : {
+          id : '',
+          name : ''
+        },
+        headers: [
+          { text: 'ID', value: 'id' },
+          { text: 'Name', value: 'name' },
+          { text: 'Actions', value: 'actions', sortable: false },
+        ],
+        
+      }
+    },
+    created() {
+      this.fetchPrograms();
+    },
+    methods: {
+      fetchPrograms() {
+        axios.get('/api/programs', this.config)
+        .then(res => this.programs = res.data)
+        .catch(err => console.log(err))
+      },
+      deleteProgram(id) {
+        axios.delete('/api/programs/' + id)
+        .then(res => {
+          this.fetchPrograms()
+          this.message = "Selected program deleted successfully"
+          this.snackbar = true        
+        }).catch(err => console.log(err))
+      },
+      editProgram(id) {
+        axios.get('/api/programs/' + id)
+        .then(res => {
+          this.program = res.data
+        })
+        .catch(err => console.log(err))
+      }
+    } 
+  }
+</script>
---
+++ resources/js/components/admin/children/AddProgram.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-row justify="center">
+
+    <v-dialog
+      v-model="dialog"
+      max-width="300"
+      persistent
+    >
+      <v-card>
+         <v-card>
+        <v-card-title class="headline" v-show="!edit">Program</v-card-title>
+        <v-card-title class="headline" v-show="edit">Edit Program</v-card-title>
+
+        <v-card-text>
+
+            <v-text-field
+                name="name"
+                label="Name of the Program"
+                id="id"
+                v-model="program.name"
+            ></v-text-field> 
+
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="hideDialog()"
+          >
+            Close
+          </v-btn>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="!edit ? addProgram() : updateProgram()"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+        
+      </v-card>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    props : ['showDialog', 'edit', 'program1'],
+    data () {
+      return {
+      
+        program2 : {
+            id : '',
+            name : ''
+        },
+      }
+      
+    },
+    computed : {
+      dialog() {
+          if(this.showDialog) {
+              return this.showDialog
+          } else {
+              return false
+          }
+      },
+
+      program() {
+          if(this.program1) {
+              return this.program1
+          } else {
+              return program2
+          }
+      },
+     
+    },
+    methods : {
+        hideDialog() {
+            this.program.name = ''
+            this.$parent.$emit('hide_dialog')
+        },
+        addProgram() {
+            axios.post('/api/programs', this.program)
+            .then(res => {
+                this.program.name = ''
+                this.$parent.$emit('program_added', 'Program added successful.')
+            }).catch(err => console.log(err))
+        },
+        updateProgram() {
+            axios.put('/api/programs/' + this.program.id, this.program)
+            .then(res => {
+                this.program.name = ''
+                this.$parent.$emit('program_added', 'Program updated successful.')
+            }).catch(err => console.log(err))
+        }
+    }
+    
+  }
+</script>
