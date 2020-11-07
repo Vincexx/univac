a6d829ac18c852a3c6e6662160eb00c1a67798d9
Vincexx <cvpitagan@gmail.com>
2020-11-07 21:42:23 +0800

    debug

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
@@ -229,7 +228,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
-     
     ],
 
 ];
